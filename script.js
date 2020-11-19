da35f7df9c7aa22458184785367aa9be1d7d266b
kytaylor <ky.taylor5468@gmail.com>
2020-11-18 17:34:40 -0800

    Adds arrays with possible characters for password in js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Character Arrays
+var passLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var passUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var passNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var passSpecialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "{", "}", "~", "`", "|"]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
